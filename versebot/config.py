3356fb4647320cf10fb7e9ab3ebbf81e3e7da5f9
Adam Grieger <adamgrieger@gmail.com>
2015-11-05 15:28:22 -0800

    Changed DATABASE_URL to DATABASE_PATH

--- versebot/config.py
+++ versebot/config.py
@@ -6,11 +6,11 @@ Copyright (c) 2015 Matthieu Grieger (MIT License)
 """
 
 from os import environ
-from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
+from logging import INFO
 
 REDDIT_USERNAME = environ["REDDIT_USERNAME"]
 REDDIT_PASSWORD = environ["REDDIT_PASSWORD"]
-DATABASE_URL = environ["DATABASE_URL"]
+DATABASE_PATH = environ["DATABASE_PATH"]
 VERSEBOT_ADMIN = environ["VERSEBOT_ADMIN"]
 LOG_LEVEL = INFO
 MAXIMUM_MESSAGE_LENGTH = 6000
