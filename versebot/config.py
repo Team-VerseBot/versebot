ce35056d928d2b074ec43168bd350dae4d23971f
Adam Grieger <adamgrieger@gmail.com>
2015-11-19 11:46:34 -0800

    Added Slack message to config.py

--- versebot/config.py
+++ versebot/config.py
@@ -8,8 +8,17 @@ Copyright (c) 2015 Matthieu Grieger (MIT License)
 from os import environ
 from logging import INFO
 
+# VerseBot
 REDDIT_USERNAME = environ["REDDIT_USERNAME"]
 DATABASE_PATH = environ["DATABASE_PATH"]
 VERSEBOT_ADMIN = environ["VERSEBOT_ADMIN"]
 LOG_LEVEL = INFO
 MAXIMUM_MESSAGE_LENGTH = 6000
+
+# Slack
+# The Team VerseBot's bot is called St. Gabriel the Archangel, so this
+# message makes sense. Customize it as you see fit for your own bot if you
+# are running your own instance of VerseBot.
+SLACK_MESSAGE = "I am Gabriel. I stand in the presence of God, and I have " \
+                "been sent to speak to you and to bring you this good news: " \
+                "VerseBot is down because of an unhandled exception."
